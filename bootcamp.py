0e29e2e64027d6d3ea4a8bd0f07f278ff28fb714
bensuyergin <67907752+bensuyergin@users.noreply.github.com>
2020-07-11 01:21:59 +0300

    Add files via upload

---
+++ bootcamp.py
@@ -0,0 +1,29 @@
+import ...
+class Information():
+    def __init__(self,data):
+    def info(self):
+class Visualizer():
+    def __init__(self):
+    def histogram(self):
+    def boxplot(self):
+    def barplot(self):
+    def heatmap(self):
+    def scatter(self):
+class PreProcessing():
+    def __init__(self):
+    def fillna(self):
+    def cleaning(self):
+    def dummy_encoding(self):
+    def feature_eng(self):
+    ....
+class model():
+class UnSupervised(model)
+    def clustering(self):#kmeans,gaussianmixturemodel,...
+class Supervised(model)
+    def regression(self):#lineer,decisiontrees,..
+    def classification(self):#nearestneighbor,..
+class Gridsearch():
+    def __init__(self):
+    def show_result(self):
+
+
